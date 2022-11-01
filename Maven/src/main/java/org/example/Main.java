ab9342982eb731c2d7f1f03a92e6740556f4a99d
Gabriel <prismaticnetwork12@gmail.com>
2022-11-01 20:07:24 -0300

    feat: understanding JPA under the hood

--- Maven/src/main/java/org/example/Main.java
+++ Maven/src/main/java/org/example/Main.java
@@ -2,13 +2,28 @@ package org.example;
 
 import org.example.Model.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
   public static void main(String[] args) {
-    Person person1 = new Person(1, "Carl", "carl@gmail.com");
-    Person person2 = new Person(2, "Carl2", "carl2@gmail.com");
-    Person person3 = new Person(3, "Carl3", "carl3@gmail.com");
-    System.out.println(person1);
-    System.out.println(person2);
-    System.out.println(person3);
+//    Person person1 = new Person(null, "Carl", "carl@gmail.com");
+//    Person person2 = new Person(null, "Carl2", "carl2@gmail.com");
+//    Person person3 = new Person(null, "Carl3", "carl3@gmail.com");
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    //always to include a transaction if your "query" is not a simple SELECT * FROM table (adding, removing or updating)
+//    entityManager.getTransaction().begin();
+//    entityManager.persist(person1);
+//    entityManager.persist(person2);
+//    entityManager.persist(person3);
+//    entityManager.getTransaction().commit();
+//    System.out.println("Inserted");
+    //finding by ID
+    Person person = entityManager.find(Person.class, 2);
+    System.out.println(person);
+    entityManager.close();
+    entityManagerFactory.close();
   }
 }
